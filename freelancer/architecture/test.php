6efac5fb169059a53c5266fdfff18263a3482db8
allenross356 <allenross653@gmail.com>
2018-01-25 13:39:36 +0530

    v0.0.6

--- freelancer/architecture/test.php
+++ freelancer/architecture/test.php
@@ -1,97 +1,132 @@
 <?php
 
-	function debug_print($obj)
+	class client
 	{
-		global $debug_mode_on;
-		if($debug_mode_on===false) return;
-		if(is_object($obj)) var_dump($obj);	else echo "$obj<br>";
-	}
-	
-	function get_sql_connection()
-	{
-		$conn = new mysqli("localhost", "root", "", "freelancer");
-		if($conn->connect_error)
+		public static $debug_mode_on=true;
+		public static $conn;
+		
+		public static function debug_print($obj)
 		{
-			echo "Connection failed: " . $conn->connect_error . "<br>";
-			die("Connection failed: " . $conn->connect_error);			
+			if(client::$debug_mode_on===false) return;
+			if(is_object($obj)) var_dump($obj);	else echo "$obj<br>";
 		}
-		return $conn;
-	}
-	
-	function close_sql_connection($conn)
-	{
-		$conn->close();
-	}
-	
-	function create_user_table()
-	{
-		global $conn;
-		$sql="CREATE TABLE client(
-					id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-					email varchar(64) not null unique,
-					pass text,
-					name text,
-					PRIMARY KEY (id)
-					);";
-		$f=$conn->query($sql);
-		if($f===true) return true;
-		return false;
-	}
-	
-	function delete_user_table()
-	{
-		global $conn;
-		$sql="drop table client";
-		$f=$conn->query($sql);
-		if($f===true) return true;
-		return false;
-	}
-	
-	function init()
-	{
-		delete_user_table();
-		create_user_table();
-	}
-	
-	function add_client($name,$email,$pass)
-	{
-		global $conn;
-		if(filter_var($email,FILTER_VALIDATE_EMAIL)===false) return false;
-		$sql="insert into client(name,email,pass) values('$name','$email','$pass')";
-		$r=$conn->query($sql);
-		if($r===true) return true;
-		debug_print($conn->error);
-		return false;
-	}
-	
-	function register_client()
-	{
 		
+		public static function get_sql_connection()
+		{
+			$conn = new mysqli("localhost", "root", "", "freelancer");
+			if($conn->connect_error)
+			{
+				echo "Connection failed: " . $conn->connect_error . "<br>";
+				die("Connection failed: " . $conn->connect_error);			
+			}
+			return $conn;
+		}
+		
+		public static function close_sql_connection($conn)
+		{
+			$conn->close();
+		}
+				
+		public static function create_user_table()
+		{
+			$sql="CREATE TABLE client(
+						id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+						email varchar(64) not null unique,
+						pass text,
+						name text,
+						PRIMARY KEY (id)
+						);";
+			$f=client::$conn->query($sql);
+			if($f===true) return true;
+			return $conn->error;
+		}
+		
+		public static function delete_user_table()
+		{ 
+			$sql="drop table client";
+			$f=client::$conn->query($sql);
+			if($f===true) return true;
+			return $conn->error;
+		}
+		
+		public static function init()
+		{
+			client::debug_print(client::delete_user_table());
+			client::debug_print(client::create_user_table());
+		}
+		
+		public static function add_client($name,$email,$pass)
+		{
+			if(filter_var($email,FILTER_VALIDATE_EMAIL)===false) return "Invalid Email Format";
+			$sql="insert into client(name,email,pass) values('$name','$email','$pass')";
+			$r=client::$conn->query($sql);
+			if($r===true) return true;
+			client::debug_print(client::$conn->error);
+			return client::$conn->error;
+		}
+		
+		public static function authenticate_client($email,$pass)
+		{
+			$sql="select id from client where email='$email' and pass='$pass'";
+			$r=client::$conn->query($sql);
+			if($r===false) return $conn->error;
+			if($r->num_rows) return true;
+			return false;
+		}
+		
+		public static function register_client($name,$email,$pass)
+		{
+			$r=add_client($name,$email,$pass);
+			if($r!==true) return $r;
+			$_SESSION['email']=$email;
+			return $r;
+		}
+		
+		public static function login_client($email,$pass)
+		{
+			if(!client::authenticate_client($email,$pass)) return false;
+			$_SESSION['email']=$email;
+		}
+		
+		public static function logout_client()
+		{
+			session_destroy();
+		}
+		
+		public static function current_client()
+		{
+			if(isset($_SESSION['email'])) return $_SESSION['email'];
+			return false;
+		}
+		
+		public function __invoke()
+		{
+			client::$debug_mode_on=true;
+			client::$conn=client::get_sql_connection();
+			session_start();
+			//client::init();
+			//client::add_client("allen","allenross653@gmail.com","allenpass");
+			//client::add_client("joseph","joseph@gmail.com","joseph");
+			//client::login_client("joseph@gmail.com","joseph");
+			//var_dump(client::current_client());
+			//client::logout_client();
+			client::close_sql_connection(client::$conn);
+		}	
 	}
+	
+	//$a=new client;
+	//$a();
 
-	function does_client_exist()
+	class a
 	{
 		
 	}
 	
-	function authenticate_client()
+	class b
 	{
-		
+		public $s=[];
+		public $n=23;
+		//public 
 	}
-	
-	function login_client()
-	{
-		
-	}
-	
-	
-	$debug_mode_on=true;
-	$conn=get_sql_connection();
-	//init();
-	add_client("allen","allenross653@gmail.com","allenpass");
-	add_client("joseph","joseph@gmail.com","joseph");
-	close_sql_connection($conn);
-
-
 
 ?>
