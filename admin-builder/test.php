2782a0e250546d5550d25fd8513428d3f96fb3aa
allenross356 <allenross653@gmail.com>
2018-01-26 14:07:46 +0530

    0.0.8

--- admin-builder/test.php
+++ admin-builder/test.php
@@ -5,10 +5,15 @@
 	//difference between mark and insert-action .. is same as mutex and semaphore.. mark action can hold only 1,unique record of its kind while insert-action can hold only multiple record.
 	//'on' and 'to' are interchangeable.. these keywords also act as separator.. any words that are not defined in the compiler act as separators
 	//create attribute/user/action type
+	//create view
 	//any name of type or attribute can hold alphabets, numbers, -, _ and start can start with alphabet or - or _.. on compilation, all '-' characters will converted to "_" with default compiler setting.. however compiler setting can changed to treat '-' to mark capitalization in which case case a variable name/type/attribute insert-record will change insertRecord on compilation.. compiler setting can set to treat the starting '-' character and middle '-' for example insert-record will convert to insertRecord but -record will convert to _record.. 
 	//: denotes begin of multi line command.. each : must be followed by ; in subsequent line.. ; denotes end of multi line command
 	//'define as' denotes begin pointer to function
+	//start and end keywords
+	//view 
 
+	
+	
 	create attribute type currency(string) possible values(USD,INR,CAD,GBP,AUD)
 	create attribute type budget-range with attributes(int,int) min,max
 	create attribute type balance with attributes(int,currency) amount,currency	
@@ -27,6 +32,9 @@
 		//OR create action type register by user usual register
 	create action type login by user define as usual login
 	
+	//optional
+	start feature freelancer
+	//end optional
 	create action type create-project by client on project define as usual insert-record
 	create action type destroy-project by client on own project define as usual delete-record
 	create action type view-project by client on own project define as select-record
@@ -43,7 +51,12 @@
 	create action type accept by freelancer on own bid on award define as usual mark
 	create action type reject by freelancer on own bid on award define as usual unmark award
 	
-	create action type send-message between user on conditions:
+	//optional
+	feature end
+	//end optional
+	
+	//<TODO>
+	activate feature usual send-message between client and freelancer with conditions:
 		//OR create action type send-message between user and user on conditions:
 	(
 		if from user is client and to user is freelancer and (to user place bid on from user project)
@@ -53,5 +66,18 @@
 	)
 	define as usual send-message;
 	create action type fetch-messages by user 
+	//end <TODO>
+	
+	
+	
+	start init view
+	logo=../images/logo.png
+	title=Better Than Freelancer
+	init end
+	
+	create view register function usual register
+	create view login function usual login
+	create view search-projects
+	
 	
 ?>
