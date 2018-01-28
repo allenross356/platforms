9d35e26ff3225d83b0b354f5570802d6bc2788a5
allenross356 <allenross653@gmail.com>
2018-01-28 21:17:36 +0530

    v0.0.16

---
+++ admin-builder/update.php
@@ -0,0 +1,33 @@
+<?php
+	
+function _get_url(){ return "<TODO>"}
+
+$connection = ssh2_connect("myserver.com", 22);
+if(ssh2_auth_password($connection, "username", "password")){
+echo "connection is authenticated";
+}
+else{
+echo "failed!";
+}
+
+
+	function sign_in_and_fetch()
+	{
+
+	}
+
+	function check_for_updates()
+	{
+
+	}
+
+	function update_package()
+	{
+		//check for updates
+		//if updates are available, 
+			//download
+			//unzip
+			//execute
+	}
+
+?>
